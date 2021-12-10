377549afdd06d885b8086a1ce37f9f36dce577ad
jacobbins <jacob@430am.co.nz>
2021-12-10 15:43:07 +1300

    first_commit

---
+++ demos/brownie_fund_me/scripts/deploy.py
@@ -0,0 +1,25 @@
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import deploy_mocks, get_account, deploy_mocks
+
+
+def deploy_fund_me():
+    account = get_account()
+
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+
+    print(f"Contract deploed to {fund_me.address}")
+
+
+def main():
+    deploy_fund_me()
